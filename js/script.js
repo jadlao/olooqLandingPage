551fde354d692adad73ac14824d3df4fe466f6ae
Josef Adlao <jadlao@gmail.com>
2018-06-03 13:29:17 +1000

    changed countdown time

--- js/script.js
+++ js/script.js
@@ -73,7 +73,7 @@ inView('.section5').on('enter', function(){
 
 // Countdown
 // Set date for launch
-var countdownDate = new Date('Jan 1, 2018 00:00:00').getTime();
+var countdownDate = new Date('Jan 1, 2020 00:00:00').getTime();
 
 var countDown = setInterval(function(){
     // Today's date and time
